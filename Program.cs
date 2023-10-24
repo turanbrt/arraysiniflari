21c91d6a3026f866c59c5527f1b1552fa8fc3380
turan <mturan.barut@gmail.com>
2023-10-24 14:32:11 +0300

    c#101--array sinifi metotları

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace arraysiniflari;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] dizi={25,45,788,5125,66,8456};
+
+        //Array.Sort
+        Console.WriteLine("*****ArraySort******");
+        Console.WriteLine("***sırasız dizi:");
+        foreach(var sayi in dizi){
+            Console.WriteLine(sayi);
+        }
+        Console.WriteLine("***sıralı dizi");
+        Array.Sort(dizi);
+
+        foreach(var sayi in dizi){
+            Console.WriteLine(sayi);
+        }
+        //Array.Clear
+        Console.WriteLine("*****ArrayClear******");
+        Array.Clear(dizi, 2,2);
+        foreach(var sayi in dizi)
+           Console.WriteLine(sayi);
+
+        // Array.reverse
+        Console.WriteLine("*****ArrayReverse******");
+        Array.Reverse(dizi);
+        foreach(var sayi in dizi)   Console.WriteLine(sayi);
+
+        // Araay.IndexOf
+        Console.WriteLine("*****ArrayIndexOf******");
+        Console.WriteLine(Array.IndexOf(dizi,45));
+        // Resize
+        Console.WriteLine("*****Resizef******");
+        Array.Resize<int>( ref dizi,9);
+        dizi[6]=22;
+        foreach(var sayi in dizi)   Console.WriteLine(sayi);
+
+
+    }
+}
